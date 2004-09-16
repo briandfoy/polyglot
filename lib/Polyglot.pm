516489ea9b7b912123d96714c08f474ca7590be0
brian d foy <brian.d.foy@gmail.com>
2004-09-16 02:26:48 +0000

    * various dist clean-ups
    * got rid of File::Find::Rule
    
    * added META.yml and README
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1472 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Polyglot.pm
+++ lib/Polyglot.pm
@@ -56,11 +56,11 @@ use autouse 'Data::Dumper' => 'Dumper';
 use Carp qw(carp);
 use Text::ParseWords qw( quotewords );
 
-$VERSION = '0.07';
+$VERSION = '0.10';
 
 my $Debug = $ENV{DEBUG} || 0;
 
-=head2 METHODS
+=head2 Methods
 
 =over 4
 
@@ -135,7 +135,7 @@ sub run
 
 		print "$prompt" if $ARGV eq '-';
 		}
-		
+
 	print "\n";
 	}
 
@@ -341,18 +341,18 @@ access a Singleton object stored as class data.
 This source is part of a SourceForge project which always has the
 latest sources in CVS, as well as all of the previous releases.
 
-        https://sourceforge.net/projects/brian-d-foy/
+        http://sourceforge.net/projects/brian-d-foy/
 
 If, for some reason, I disappear from the world, one of the other
 members of the project can shepherd this module appropriately.
 
 =head1 AUTHOR
 
-brian d foy, E<lt>bdfoy@cpan.orgE<gt>.
+brian d foy, C<< <bdfoy@cpan.org> >>.
 
 =head1 COPYRIGHT and LICENSE
 
-Copyright 2003, brian d foy, All rights reserved
+Copyright 2004, brian d foy, All rights reserved
 
 This software is available under the same terms as perl.
 
--- t/load.t
+++ t/load.t
@@ -1,12 +1,7 @@
 # $Id$
 BEGIN {
 	use File::Find::Rule;
-	@classes = map { my $x = $_;
-		$x =~ s|^blib/lib/||;
-		$x =~ s|/|::|g;
-		$x =~ s|\.pm$||;
-		$x;
-		} File::Find::Rule->file()->name( '*.pm' )->in( 'blib/lib' );
+	@classes = qw(Polyglot);
 	}
 
 use Test::Builder;
