af39fb181ca3909d98c8d5429d6c7d8d31430f19
brian d foy <brian.d.foy@gmail.com>
2008-01-18 10:42:03 +0000

    turn on (then off) warnings
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@2504 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Polyglot.pm
+++ lib/Polyglot.pm
@@ -3,6 +3,9 @@ package Polyglot;
 use strict;
 use vars qw($VERSION);
 
+use warnings;
+no warnings;
+
 =head1 NAME
 
 Polyglot - a little language interpreter
@@ -56,7 +59,7 @@ use autouse 'Data::Dumper' => 'Dumper';
 use Carp qw(carp);
 use Text::ParseWords qw( quotewords );
 
-$VERSION = '0.13';
+$VERSION = '0.14';
 
 my $Debug = $ENV{DEBUG} || 0;
 
@@ -357,7 +360,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2007 brian d foy.  All rights reserved.
+Copyright (c) 2002-2008 brian d foy.  All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
