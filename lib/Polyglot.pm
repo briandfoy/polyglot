3fb371ea2af2ec021a4943b37153e72ad0823ae3
brian d foy <brian.d.foy@gmail.com>
2016-02-02 20:58:05 -0500

    Update GitHub address

--- lib/Polyglot.pm
+++ lib/Polyglot.pm
@@ -347,13 +347,9 @@ access a Singleton object stored as class data.
 
 =head1 SOURCE AVAILABILITY
 
-This source is part of a SourceForge project which always has the
-latest sources in CVS, as well as all of the previous releases.
+The source is in GitHub:
 
-        http://sourceforge.net/projects/brian-d-foy/
-
-If, for some reason, I disappear from the world, one of the other
-members of the project can shepherd this module appropriately.
+	https://github.com/briandfoy/polyglot
 
 =head1 AUTHOR
 
